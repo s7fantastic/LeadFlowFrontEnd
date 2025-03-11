83f27c25f13a0097ae8abd51d96f930ebc039c98
saeed <s7fantastic@gmail.com>
2025-03-12 03:00:05 +0330

    Deploy V2

--- src/app/leads/add/page.tsx
+++ src/app/leads/add/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-const API_URL = "https://leadflowbackend.onrender.com/api/lead/add";
+const API_URL = "https://leadflowbackend.onrender.com/api/lead";
 
 export default function AddLeadPage() {
     const router = useRouter();
